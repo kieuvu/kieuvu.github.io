771c81e0648ff7adcde3874a3c282f2e5e6c0bba
“kieuvu” <kieuvuvuvu@gmail.com>
2023-03-26 21:32:17 +0700

    convert to oop code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,67 @@
-// Web API
-const synth = window.speechSynthesis;
+class Flashcard {
+    constructor(data) {
+        this.data = data;
+        this.cardIndex = 0;
+        this.synth = window.speechSynthesis;
+        this.imgSection = document.getElementById("image");
+        this.prevButton = document.getElementById("navigation_prev");
+        this.nextButton = document.getElementById("navigation_next");
+        this.currentCard = document.getElementById("current_card");
+        this.questionButton = document.getElementById("audio_question");
+        this.answerButton = document.getElementById("audio_answer");
+        this.toast = Swal.mixin({
+            toast: true,
+            position: "center",
+            timer: 3000,
+            showConfirmButton: false,
+        });
+        this.renderCard();
+        this.questionButton.addEventListener("click", this.sayQuestion.bind(this));
+        this.answerButton.addEventListener("click", this.sayAnswer.bind(this));
+        this.prevButton.addEventListener("click", this.prevCard.bind(this));
+        this.nextButton.addEventListener("click", this.nextCard.bind(this));
+    }
 
-// DOM
-const imgSection = document.getElementById("image");
-const prevButton = document.getElementById("navigation_prev");
-const nextButton = document.getElementById("navigation_next");
-const currentCard = document.getElementById("current_card");
-const questionButton = document.getElementById("audio_question");
-const answerButton = document.getElementById("audio_answer");
+    speak(text, text_vi) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.rate = 0.7;
+        this.synth.speak(utterance);
 
-// Handle
-let cardIndex = 0;
+        this.toast.fire({
+            title: text_vi,
+        });
 
-window.onload = () => {
-    renderCard();
-};
+        utterance.addEventListener("start", () => {
+            Swal.stopTimer;
+        });
 
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'center',
-    timer: 3000,
-    showConfirmButton: false,
-});
+        utterance.addEventListener("end", () => {
+            Swal.resumeTimer;
+        });
+    }
 
-const speak = (text, text_vi) => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.rate = 0.7;
-    synth.speak(utterance);
+    renderCard() {
+        this.currentCard.innerText = this.data[this.cardIndex].id;
+        this.imgSection.innerHTML = `<img src="${this.data[this.cardIndex].img}"/>`;
+    }
 
-    Toast.fire({
-        title: text_vi
-    });
+    sayQuestion() {
+        this.speak(this.data[this.cardIndex].ques, this.data[this.cardIndex].ques_vi);
+    }
 
-    utterance.addEventListener('start', () => {
-        Swal.stopTimer;
-    });
+    sayAnswer() {
+        this.speak(this.data[this.cardIndex].answ, this.data[this.cardIndex].answ_vi);
+    }
 
-    utterance.addEventListener('end', () => {
-        Swal.resumeTimer;
-    });
-};
+    nextCard() {
+        this.cardIndex = (this.cardIndex == this.data.length - 1) ? 0 : ++this.cardIndex;
+        this.renderCard();
+    }
 
-const renderCard = () => {
-    currentCard.innerText = data[cardIndex].id;
-    imgSection.innerHTML = `
-        <img src="${data[cardIndex].img}"/>
-    `;
-};
+    prevCard() {
+        this.cardIndex = (this.cardIndex == 0) ? this.data.length - 1 : --this.cardIndex;
+        this.renderCard();
+    }
+}
 
-const sayQuestion = () => {
-    speak(data[cardIndex].ques, data[cardIndex].ques_vi);
-};
-
-const sayAnswer = () => {
-    speak(data[cardIndex].answ, data[cardIndex].answ_vi);
-};
-
-const nextCard = () => {
-    cardIndex = (cardIndex == data.length - 1) ? 0 : ++cardIndex;
-    renderCard();
-};
-
-const prevCard = () => {
-    cardIndex = (cardIndex == 0) ? data.length - 1 : --cardIndex;
-    renderCard();
-};
+const flashcard = new Flashcard(data);
